6d87efa7637e3291efdc8c4a6619b952a2a52be5
SekaiMoe <153579552+SekaiMoe@users.noreply.github.com>
2024-06-19 15:12:14 +0800

    init

--- recovery/root/system/bin/postrecoveryboot.sh
+++ recovery/root/system/bin/postrecoveryboot.sh
@@ -1,10 +1,14 @@
 #!/sbin/sh
 #
-# TODO: this kludge is needed to prevent issues with mounting
-# system and vendor in some zip installers and in the gui
+# WIP
 #
-insmod /vendor/lib/modules/1.1/xiaomi_touch.ko
-insmod /vendor/lib/modules/1.1/goodix_core.ko
-insmod /vendor/lib/modules/1.1/focaltech_touch.ko
+
+insmod /vendor/lib/modules/
+insmod /vendor/lib/modules/adsp_loader_dlkm.ko
+insmod /vendor/lib/modules/gpr_dlkm.ko
+insmod /vendor/lib/modules/q6_notifier_dlkm.ko
+insmod /vendor/lib/modules/q6_pdr_dlkm.ko
+insmod /vendor/lib/modules/snd_event_dlkm.ko
+insmod /vendor/lib/modules/spf_core_dlkm.ko
+
 exit 0
-#
